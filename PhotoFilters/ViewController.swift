aee6e70151db893544beac9398f4acce57ab3d18
beforeload <fe.daniel91@gmail.com>
2014-10-12 22:08:46 +0800

    init the demo

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    let context = CIContext(options: nil)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func applyFilter(sender: UIBarButtonItem) {
+        let inputImage = CIImage(image: photoImageView.image)
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        let filterImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        let renderImage = context.createCGImage(filterImage, fromRect: filterImage.extent())
+        photoImageView.image = UIImage(CGImage: renderImage)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
